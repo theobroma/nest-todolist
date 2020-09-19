288fdf62f6bcca605d013e55005ffb65a147eaeb
Alexandr Siryj <theobroma222@gmail.com>
2020-09-19 06:55:01 +0300

    swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (!configService.isProduction()) {
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Item API')
+        .setDescription('My Item API')
+        .build(),
+    );
+    // set URL - 'localhost:3000/api/'
+    http: SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();

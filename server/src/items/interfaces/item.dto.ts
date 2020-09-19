8e428994536cc991ae6ee6e853ff47dd37af32c0
Alexandr Siryj <theobroma222@gmail.com>
2020-09-19 07:49:07 +0300

    dto swagger

--- server/src/items/interfaces/item.dto.ts
+++ server/src/items/interfaces/item.dto.ts
@@ -1,5 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsBoolean } from 'class-validator';
+
 export class ItemDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   readonly id: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   readonly text: string;
+  @ApiProperty()
+  @IsBoolean()
+  @IsNotEmpty()
   readonly completed: boolean;
 }
--- server/src/items/items.controller.ts
+++ server/src/items/items.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ItemDto } from './interfaces/item.dto';
 
+@ApiTags('items')
 @Controller('items')
 export class ItemsController {
   @Get()
@@ -8,6 +10,12 @@ export class ItemsController {
     return 'we get all items';
   }
   @Post()
+  @ApiOperation({ summary: 'Create item' })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid request.' })
   create(@Body() itemDto: ItemDto) {
     return itemDto;
   }

573fa3a11bb105a0cd332c9466f6ec3e7b395b77
Alexandr Siryj <theobroma222@gmail.com>
2020-09-19 05:53:57 +0300

    itemDto

---
+++ server/src/items/interfaces/item.dto.ts
@@ -0,0 +1,5 @@
+export class ItemDto {
+  readonly id: string;
+  readonly text: string;
+  readonly completed: boolean;
+}
--- server/src/items/items.controller.ts
+++ server/src/items/items.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemDto } from './interfaces/item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   getItems() {
     return 'we get all items';
+  }
+  @Post()
+  create(@Body() itemDto: ItemDto) {
+    return itemDto;
   }
 }

56e5c97cfe76b7a52227916264b49b225e856814
Alexandr Siryj <theobroma222@gmail.com>
2020-10-09 04:42:36 +0300

    api key strategy

--- server/src/migration/1600725022123-CreateItems.ts
+++ server/src/migration/1600725022123-CreateItems.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateItems1600725022123 implements MigrationInterface {
-    name = 'CreateItems1600725022123'
+  name = 'CreateItems1600725022123';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "item" ("id" SERIAL NOT NULL, "text" character varying NOT NULL, "completed" boolean, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "item" ("id" SERIAL NOT NULL, "text" character varying NOT NULL, "completed" boolean, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "item"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "item"`);
+  }
 }
